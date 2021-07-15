8fb643e9e130b4c32e66fb51b2ca66d7d1725ca2
diorvalend55 <w.valendrick@gmail.com>
2021-07-15 13:50:52 +0800

    create Rooms

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
     <Navigation :user='user' @logout='logout' />
-    <router-view :user='user' @logout='logout' />
+    <router-view 
+    :user='user'
+    :rooms='rooms'
+    @logout='logout'
+    @addRoom='addRoom'
+    @deleteRoom='deleteRoom'
+    @checkIn='checkIn' />
   </div>
 </template>
 
@@ -13,7 +19,8 @@ export default {
   name: 'App',
   data: function () {
     return {
-      user: null
+      user: null,
+      rooms: []
     }
   },
   methods: {
@@ -23,12 +30,68 @@ export default {
         this.user = null
         this.$router.push('login')
       })
+    },
+    addRoom: function(payload) {
+      db.collection('users')
+      .doc(this.user.uid)
+      .collection('rooms')
+      .add({
+        name: payload,
+        createdAt: Firebase.firestore.FieldValue.serverTimestamp()
+      })
+    },
+    deleteRoom: function(payload) {
+      db.collection('users')
+      .doc(this.user.uid)
+      .collection('rooms')
+      .doc(payload)
+      .delete()
+    },
+    checkIn: function(payload) {
+      const roomRef = db
+        .collection('users')
+        .doc(payload.hostID)
+        .collection('rooms')
+        .doc(payload.roomID)
+
+      roomRef.get()
+        .then(doc => {
+          if (doc.exists) {
+            roomRef
+            .collection('attendees')
+            .doc(this.user.uid)
+            .set({
+              displayName: payload.displayName,
+              createdAt: Firebase.firestore.FieldValue.serverTimestamp()
+            })
+            .then(() => this.$router.push('/'))
+          }
+        })
     }
   },
   mounted() {
     Firebase.auth().onAuthStateChanged(user => {
       if (user) {
         this.user = user
+        db.collection('users')
+          .doc(this.user.uid)
+          .collection('rooms')
+          .onSnapshot( snapshot => {
+            const snapData = []
+            snapshot.forEach(doc => {
+              snapData.push({
+                id: doc.id,
+                name: doc.data().name
+              })
+            })
+            this.rooms = snapData.sort((a,b) => {
+              if (a.name.toLowerCase() < b.name.toLowerCase()) {
+                return -1
+              } else {
+                return 1
+              }
+            })
+          })
       }
     })
   },
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 
+import {library} from '@fortawesome/fontawesome-svg-core'
+import { faTrash, faVideo, faUser } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faTrash, faVideo, faUser)
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app');
