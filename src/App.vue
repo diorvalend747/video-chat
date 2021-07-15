e99952d732c5bb43c431f6ab01de59ba52f003a4
diorvalend55 <w.valendrick@gmail.com>
2021-07-15 12:46:00 +0800

    registration and authentication

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,41 @@
 <template>
   <div id="app">
+    <Navigation :user='user' @logout='logout' />
+    <router-view :user='user' @logout='logout' />
   </div>
 </template>
 
 <script>
+import db from './db.js'
+import Firebase from 'firebase'
+import Navigation from './components/Navigation.vue'
+export default {
+  name: 'App',
+  data: function () {
+    return {
+      user: null
+    }
+  },
+  methods: {
+    logout: function() {
+      Firebase.auth().signOut()
+      .then(() => {
+        this.user = null
+        this.$router.push('login')
+      })
+    }
+  },
+  mounted() {
+    Firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.user = user
+      }
+    })
+  },
+  components: {
+    Navigation
+  }
+}
 </script>
 
 <style lang="scss">
